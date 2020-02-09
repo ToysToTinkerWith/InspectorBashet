8403018e0dcd3cc1e875b9f81f0a66468a020150
halll8@wwu.edu <halll8@wwu.edu>
2020-02-08 18:17:44 -0800

    Added basic skeleton for player js class

---
+++ js/Player.js
@@ -0,0 +1,15 @@
+class Player {
+
+  constructor (name, age) {
+    this.name = name;
+    this.age = age;
+  }
+
+  get name () {
+    return this.name;
+  }
+
+  get age () {
+    return this.age;
+  }
+}
